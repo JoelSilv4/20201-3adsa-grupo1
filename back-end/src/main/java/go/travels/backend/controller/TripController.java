b984cee903fe5cc370871f761cad0180ec38519c
kaiquepill <kaique.gomes@bandtec.com.br>
2020-09-24 17:16:59 -0300

    criado o endpoint de GET para buscar as viagens

--- back-end/src/main/java/go/travels/backend/controller/TripController.java
+++ back-end/src/main/java/go/travels/backend/controller/TripController.java
@@ -4,11 +4,11 @@ import go.travels.backend.document.Trip;
 import go.travels.backend.dto.TripDTO;
 import go.travels.backend.services.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 
@@ -18,6 +18,8 @@ public class TripController {
 
     private SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+    private Integer qtdPorPagina = 15;
+
     @Autowired
     TripService tripService;
 
@@ -26,6 +28,22 @@ public class TripController {
         Trip trip = convertDtoforTrip(tripDTO);
         tripService.persist(trip);
         return ResponseEntity.ok().body(convertTripForDto(trip));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Page<TripDTO>> findAll(
+             @PathVariable String userId,
+             @RequestParam(value = "pag", defaultValue = "0") Integer pag,
+             @RequestParam(value = "ord", defaultValue = "id") String ord,
+             @RequestParam(value = "dir", defaultValue = "DESC") String dir){
+
+        PageRequest pageRequest = PageRequest.of(pag, qtdPorPagina, Sort.Direction.valueOf(dir), ord);
+        Page<Trip> trip = tripService.findByUserId(userId, pageRequest);
+
+        Page<TripDTO> tripDTO = trip.map(x -> convertTripForDto(x));
+
+        return ResponseEntity.ok(tripDTO);
+
     }
 
     private Trip convertDtoforTrip(TripDTO tripDTO) {
--- back-end/src/main/java/go/travels/backend/services/TripService.java
+++ back-end/src/main/java/go/travels/backend/services/TripService.java
@@ -3,6 +3,8 @@ package go.travels.backend.services;
 import go.travels.backend.document.Trip;
 import go.travels.backend.repositories.TripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +15,7 @@ public class TripService {
 
     public Trip persist(Trip trip) { return  tripRepository.save(trip); }
 
-    public Trip findByUserId(String userId) { return tripRepository.findByIdUser(userId); }
+    public Page<Trip> findByUserId(String userId, PageRequest pageRequest) { return tripRepository.findByIdUser(userId, pageRequest); }
+
+
 }
