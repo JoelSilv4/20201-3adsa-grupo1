8309badf38e11114e2b1e25aba69944efd5ba6f3
kaiquepill <kaique.gomes@bandtec.com.br>
2020-10-12 15:33:20 -0300

    fix

--- back-end/src/main/java/go/travels/backend/controller/PostController.java
+++ back-end/src/main/java/go/travels/backend/controller/PostController.java
@@ -53,18 +53,23 @@ public class PostController {
         }
     }
 
+//    @GetMapping
+//    public ResponseEntity<Page<PostDTO>> findAll(
+//            @RequestParam(value = "pag", defaultValue = "0") Integer pag,
+//            @RequestParam(value = "ord", defaultValue = "id") String ord,
+//            @RequestParam(value = "dir", defaultValue = "DESC") String dir){
+//
+//        PageRequest pageRequest = PageRequest.of(pag, qtdPorPagina, Sort.Direction.valueOf(dir), ord);
+//        Page<Post> post = postService.findAll(pageRequest);
+//
+//        Page<PostDTO> postDTOS = post.map(this::convertDocforDTO);
+//
+//        return ResponseEntity.ok(postDTOS);
+//    }
+
     @GetMapping
-    public ResponseEntity<Page<PostDTO>> findAll(
-            @RequestParam(value = "pag", defaultValue = "0") Integer pag,
-            @RequestParam(value = "ord", defaultValue = "id") String ord,
-            @RequestParam(value = "dir", defaultValue = "DESC") String dir){
-
-        PageRequest pageRequest = PageRequest.of(pag, qtdPorPagina, Sort.Direction.valueOf(dir), ord);
-        Page<Post> post = postService.findAll(pageRequest);
-
-        Page<PostDTO> postDTOS = post.map(this::convertDocforDTO);
-
-        return ResponseEntity.ok(postDTOS);
+    public ResponseEntity<?> ok(){
+        return ResponseEntity.ok("postDTOS");
     }
 
     @PostMapping("/like")
