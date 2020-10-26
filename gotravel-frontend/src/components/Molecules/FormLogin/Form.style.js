0b4c7be7496792ef6198b21757f68a873f9e29d7
lucasmessiasbandtec <lucas.messias@bandtec.com.br>
2020-10-26 17:31:35 -0300

    adiciona estilização do login

--- gotravel-frontend/src/components/Molecules/FormLogin/Form.style.js
+++ gotravel-frontend/src/components/Molecules/FormLogin/Form.style.js
@@ -1,29 +1,38 @@
 import styled from 'styled-components';
-import img from '../../../assets/foto_tela_login.png';
+
 
 export const FirstContainer = styled.div`
   display: flex;
-  height: 100vh;
   width: 100%;
   flex-direction: row;
   justify-content: center;
   align-content: center;
+  background-color: #D9FFE8;
+
+  h1 {
+    font-weight: 600;
+    font-size: 35px;
+  }
+
+  spam {
+    color: #2D73DD;
+  }
 `;
 
 export const ArtContainer = styled.div`
-  background-image: url(${img});
-  background-size: cover;
-  width: 100%;
 
-  img {
-    height: 100%;
-    width: 100%;
-  }
+  width: 100%;
+  height: 100%; 
+
+svg{
+  width: 100%;
+  height: 100%;
+}
 `;
 
 export const FormContainer = styled.div`
   display: flex;
-  width: 100%;
+  width: 90%;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -32,6 +41,17 @@ export const FormContainer = styled.div`
 export const FormStyle = styled.form`
   color: black;
 
+  input {
+    background: #FFFFFF;
+    border: 1px solid #000000;
+    box-sizing: border-box;
+    border-radius: 30px;
+    width: 350px;
+    height: 55px;
+    outline: 0;
+    padding-left: 20px;
+  }
+
   div {
     display: flex;
     flex-direction: column;
@@ -39,7 +59,24 @@ export const FormStyle = styled.form`
   }
 
   button {
-    padding: 10px 15px;
-    margin: 10px 0;
+    width: 200px;
+    height: 40px;
+    left: 913px;
+    top: 621px;
+    margin-top: 20px;
+    border-radius: 5px;
+    background-color: #F0F0F0;
+    color: black;
+    font-size: 15px;
+  }
+
+  button:first-child {
+    background-color: #2D73DD;
+    color: white;
   }
 `;
+
+export const ContainerButtons = styled.div`
+  display: flex;
+  align-items: center;
+`
--- gotravel-frontend/src/pages/institucional/institucional.js
+++ gotravel-frontend/src/pages/institucional/institucional.js
@@ -4,7 +4,6 @@ import ExternNavbar from '../../components/Organisms/ExternNavbar';
 import ContainerTop from '../../components/Organisms/ContainerTop';
 import ContainerKnowMore from '../../components/Organisms/ContainerKnowMore';
 import ContainerWhiteKnowMore from '../../components/Organisms/ContainerWhiteKnowMore';
-import Footer from '../../components/Organisms/Footer';
 
 const Institucional = () => {
   return (
@@ -12,7 +11,6 @@ const Institucional = () => {
       <ContainerTop textButton="Entrar" textColorButton="#2D73DD" />
       <ContainerKnowMore titleColorInfo="#000000" textTitleInfo="Diga onde você quer ir" textColorInfo="#FFFFFF" textInfo="Informe sua localização e o seu destino" textTitleInfo2="Filtre com base nos seus serviços favoritos" textInfo2="Selecione o que você quer encontrar pelo caminho" />
       <ContainerWhiteKnowMore titleColorInfo="#2D73DD" textTitleInfo="Adicione os estabelecimentos" textColorInfo="#000000" textInfo="Adicione-os ao seu itinerário pessoal" textTitleInfo2="Dê um GO! e explore" textInfo2="Consulte o seu planejamento quando quiser durante a viagem" />
-      <Footer />
     </Layout>
   );
 };
