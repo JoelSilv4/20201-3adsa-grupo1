7f4397cbcb92c7ef25961292751b8b5e013806ac
lucasmessiasbandtec <lucas.messias@bandtec.com.br>
2020-11-22 20:31:07 -0300

    cria arquivo da pagina de ultimas viagens

---
+++ gotravel-frontend/src/pages/viagens/viagens.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../../components/Layout'
+import ContainerViagens from '../../components/Organisms/ContainerViagens'
+
+const ultimasViagens = () => {
+
+    return (
+        <Layout>
+            <ContainerViagens />
+        </Layout>
+
+    )
+}
+
+export default ultimasViagens;
+
