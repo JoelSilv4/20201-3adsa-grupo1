e75c79d482c5ed255aef44ec2a323b6690fba5d1
unknown <henrique_barretto@outlook.com>
2020-11-26 23:07:39 -0300

    add: publicações redireciona quando usuario != logado

--- gotravel-frontend/src/pages/publicacoes/publicacoes.js
+++ gotravel-frontend/src/pages/publicacoes/publicacoes.js
@@ -6,12 +6,17 @@ import StateContext from '../../StateContext';
 import AddPost from '../../components/Molecules/AddPost';
 import FormPost from '../../components/Molecules/FormPost/FormPost';
 import SelectTrip from '../../components/Molecules/SelectTrip';
+import { Redirect } from 'react-router-dom';
 
 const Publicacoes = () => {
   const appDispatch = useContext(DispatchContext);
   appDispatch({ type: 'is-not-institutional' });
 
   const appState = useContext(StateContext);
+
+  if (!appState.logged) {
+    return <Redirect to="/auth" />;
+  }
 
   return (
     <Layout>
