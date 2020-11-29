219072a00c18e958d92b2a3907ab01ce8ef08c8c
unknown <henrique_barretto@outlook.com>
2020-11-29 17:37:19 -0300

    add: botão para acessar viagens

--- gotravel-frontend/src/components/Molecules/AddPost/index.js
+++ gotravel-frontend/src/components/Molecules/AddPost/index.js
@@ -4,6 +4,7 @@ import DispatchContext from '../../../DispatchContext';
 import svg_addbutton from '../../../assets/add_button.svg';
 import svg_listsolid from '../../../assets/list_solid.svg';
 import { Container } from './style';
+import { Redirect } from 'react-router-dom';
 
 function AddPost() {
   const appDispatch = useContext(DispatchContext);
@@ -12,13 +13,17 @@ function AddPost() {
     appDispatch({ type: 'show-form-post' });
   };
 
+  const handleTrips = () => {
+    return <Redirect to="/" />;
+  };
+
   return (
     <Container>
       <div onClick={handlePost}>
         <img src={svg_addbutton} alt="" />
         <p>Adicionar Post</p>
       </div>
-      <div className="mytrips" onClick={handlePost}>
+      <div className="mytrips" onClick={handleTrips}>
         <img src={svg_listsolid} alt="" />
         <p>Minhas Viagens</p>
       </div>
