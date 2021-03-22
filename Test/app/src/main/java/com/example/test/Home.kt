946e3f72b3ff160ce7486150449e5ba64068bf58
arthuralvaresbandtec <arthur.alvares@bandtec.com.br>
2021-03-21 23:19:11 -0300

    adicionando model  bottom sheet na página de criar post

---
+++ Test/app/src/main/java/com/example/test/Home.kt
@@ -0,0 +1,31 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class Home : Fragment() {
+
+    lateinit  var button: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        button = view.findViewById(R.id.button)
+
+        button.setOnClickListener {
+            findNavController().navigate(R.id.FirstToSecond)
+        }
+
+        return view
+    }
+
+
+}
--- Test/app/src/main/java/com/example/test/MainActivity.kt
+++ Test/app/src/main/java/com/example/test/MainActivity.kt
@@ -1,37 +1,26 @@
-package com.example.app_go
+package com.example.test
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.jaeger.library.StatusBarUtil
 
-class PostScreen : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    lateinit var navController: NavController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_screen)
-        StatusBarUtil.setTransparent(this)
-
+        setContentView(R.layout.activity_main)
 
         navController = findNavController(R.id.navHostFragment)
+        setupActionBarWithNavController(navController)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-
-    fun back_home(view: View) {
-        val intent = Intent(this, HomeScreen::class.java)
-        startActivity(intent)
-    }
-
 
 }
